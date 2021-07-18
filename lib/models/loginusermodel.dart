adcd36d127c462923a4f85dbdb1d2854ff0db46b
caroline <caroline.mcgregor.2020@uni.strath.ac.uk>
2021-07-19 00:54:00 +0100

    Initial Commit.

---
+++ lib/models/loginusermodel.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class LoginUserModel {
+  String displayName;
+  String email;
+  String photoUrl;
+
+  LoginUserModel({this.displayName, this.email, this.photoUrl});
+}
