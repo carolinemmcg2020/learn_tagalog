c088638d4fc28b6b3a1f6ea6e12f599cd56b440e
caroline <caroline.mcgregor.2020@uni.strath.ac.uk>
2021-07-19 00:54:25 +0100

    Initial Commit.

--- lib/services/loginservice.dart
+++ lib/services/loginservice.dart
@@ -1,13 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:learn_tagalog/models/loginusermodel.dart';
 
 class LoginService {
-
   LoginService() {
     Firebase.initializeApp();
   }
+
+  LoginUserModel _userModel;
+
+  LoginUserModel get loggedInUserModel => _userModel;
 
   Future<bool> signInWithGoogle() async {
     // Trigger the authentication flow
@@ -15,12 +18,13 @@ class LoginService {
 
     final GoogleSignInAccount googleUser = await googleSignIn.signIn();
 
-    if (googleUser == null){
+    if (googleUser == null) {
       return false;
     }
 
     // obtain the auth details from the request
-    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
 
     // Create a new credential
     final GoogleAuthCredential credential = GoogleAuthProvider.credential(
@@ -29,11 +33,23 @@ class LoginService {
     );
 
     // Once signed in, return the UserCredential
-    UserCredential userCreds = await FirebaseAuth.instance.signInWithCredential(credential);
-    if(userCreds != null){
+    UserCredential userCreds =
+        await FirebaseAuth.instance.signInWithCredential(credential);
+    if (userCreds != null) {
       //TODO: collect user info here
+
+      _userModel = LoginUserModel(
+        displayName: userCreds.user.displayName,
+        photoUrl: userCreds.user.photoURL,
+        email: userCreds.user.email
+      );
 
     }
     return true;
+  }
+
+  void singOut() async {
+    await GoogleSignIn().signOut();
+    _userModel = null;
   }
 }
