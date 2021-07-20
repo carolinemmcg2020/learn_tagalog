b093f88455ce1fb55e2b85807e2ac8e8d2b4d5b0
caroline <caroline.mcgregor.2020@uni.strath.ac.uk>
2021-07-20 11:32:16 +0100

    Added more categories.

--- lib/helpers/utils.dart
+++ lib/helpers/utils.dart
@@ -8,150 +8,241 @@ class Utils {
   static List<Category> getMockedCategories() {
     return [
       Category(
-          name: 'Food',
-          icon: FontAwesomeIcons.breadSlice,
-          color: Colors.purple,
-          backgroundColor: Colors.purpleAccent,
-          lessons: [
-            Lessons(
-                name: 'Food one',
-                icon: FontAwesomeIcons.hamburger,
-                color: Colors.purple,
-                backgroundColor: Colors.purpleAccent,
-                lessonContent: [
-                  LessonContent(
-                      tagalogWord: 'itlog',
-                      engWord: 'egg',
-                      icon: FontAwesomeIcons.egg,
-                      color: Colors.white,
-                      audio: 'assets/audio/itlog.mp3'),
-                  LessonContent(
-                      tagalogWord: 'hatdog',
-                      engWord: 'hotdog',
-                      icon: FontAwesomeIcons.hotdog,
-                      color: Colors.white,
-                      audio: 'assets/audio/hotdog.mp3'),
-                  //TODO import audio assets and fill out audio variable for each lesson content object
-                  LessonContent(
-                    tagalogWord: 'berger',
-                    engWord: 'burger',
-                    icon: FontAwesomeIcons.hamburger,
-                    color: Colors.white,
-                  ),
-                  LessonContent(
-                    tagalogWord: 'kanin',
-                    engWord: 'rice',
-                    icon: FontAwesomeIcons.pills,
-                    color: Colors.white,
-                  ),
-                  LessonContent(
-                    tagalogWord: 'manok',
-                    engWord: 'chicken',
+        name: 'Food',
+        icon: FontAwesomeIcons.breadSlice,
+        color: Colors.purple,
+        backgroundColor: Colors.purpleAccent,
+        lessons: [
+          Lessons(
+              name: 'Food one',
+              icon: FontAwesomeIcons.hamburger,
+              color: Colors.purple,
+              backgroundColor: Colors.purpleAccent,
+              lessonContent: [
+                LessonContent(
+                    tagalogWord: 'itlog',
+                    engWord: 'egg',
                     icon: FontAwesomeIcons.egg,
                     color: Colors.white,
-                  ),
-                ]),
-            Lessons(
-                name: 'Food two',
-                icon: FontAwesomeIcons.flask,
-                color: Colors.purple,
-                backgroundColor: Colors.purpleAccent,
-                lessonContent: [
-                  LessonContent(
-                      tagalogWord: 'Walang anuman',
-                      engWord: 'No worries, You/re Welcome',
-                      icon: FontAwesomeIcons.egg,
-                      color: Colors.white,
-                      audio: 'assets/audio/egg.mp3'),
-                  LessonContent(
-                      tagalogWord: 'Pagkain',
-                      engWord: 'Food',
-                      icon: FontAwesomeIcons.hotdog,
-                      color: Colors.white,
-                      audio: 'assets/audio/hotdog.mp3'),
-                  //TODO import audio assets and fill out audio variable for each lesson content object
-                  LessonContent(
-                    tagalogWord: 'Masarap',
-                    engWord: 'Delicious',
-                    icon: FontAwesomeIcons.hamburger,
+                    audio: 'assets/audio/itlog.mp3'),
+                LessonContent(
+                    tagalogWord: 'hatdog',
+                    engWord: 'hotdog',
+                    icon: FontAwesomeIcons.hotdog,
                     color: Colors.white,
-                  ),
-                  LessonContent(
-                    tagalogWord: 'Kain Tayo',
-                    engWord: 'Lets Eat',
-                    icon: FontAwesomeIcons.sink,
-                    color: Colors.white,
-                  ),
-                  LessonContent(
-                    tagalogWord: 'adobo',
-                    engWord: 'chicken',
+                    audio: 'assets/audio/hotdog.mp3'),
+                //TODO import audio assets and fill out audio variable for each lesson content object
+                LessonContent(
+                  tagalogWord: 'berger',
+                  engWord: 'burger',
+                  icon: FontAwesomeIcons.hamburger,
+                  color: Colors.white,
+                ),
+                LessonContent(
+                  tagalogWord: 'kanin',
+                  engWord: 'rice',
+                  icon: FontAwesomeIcons.pills,
+                  color: Colors.white,
+                ),
+                LessonContent(
+                  tagalogWord: 'manok',
+                  engWord: 'chicken',
+                  icon: FontAwesomeIcons.egg,
+                  color: Colors.white,
+                ),
+              ]),
+          Lessons(
+              name: 'Food two',
+              icon: FontAwesomeIcons.flask,
+              color: Colors.purple,
+              backgroundColor: Colors.purpleAccent,
+              lessonContent: [
+                LessonContent(
+                    tagalogWord: 'Walang anuman',
+                    engWord: 'No worries, You/re Welcome',
                     icon: FontAwesomeIcons.egg,
                     color: Colors.white,
-                  ),
-                ]),
-            Lessons(
-              name: 'Food three',
-              icon: FontAwesomeIcons.wineBottle,
-              color: Colors.purple,
-              backgroundColor: Colors.purpleAccent,
-            ),
-            Lessons(
-              name: 'Food four',
-              icon: FontAwesomeIcons.glassWhiskey,
-              color: Colors.purple,
-              backgroundColor: Colors.purpleAccent,
-            ),
-            Lessons(
-              name: 'Food five',
-              icon: FontAwesomeIcons.glassMartini,
-              color: Colors.purple,
-              backgroundColor: Colors.purpleAccent,
-            ),
-            Lessons(
-              name: 'Food six',
-              icon: FontAwesomeIcons.sink,
-              color: Colors.purple,
-              backgroundColor: Colors.purpleAccent,
-            ),
-          ]),
+                    audio: 'assets/audio/egg.mp3'),
+                LessonContent(
+                    tagalogWord: 'Pagkain',
+                    engWord: 'Food',
+                    icon: FontAwesomeIcons.hotdog,
+                    color: Colors.white,
+                    audio: 'assets/audio/hotdog.mp3'),
+                //TODO import audio assets and fill out audio variable for each lesson content object
+                LessonContent(
+                  tagalogWord: 'Masarap',
+                  engWord: 'Delicious',
+                  icon: FontAwesomeIcons.hamburger,
+                  color: Colors.white,
+                ),
+                LessonContent(
+                  tagalogWord: 'Kain Tayo',
+                  engWord: 'Lets Eat',
+                  icon: FontAwesomeIcons.sink,
+                  color: Colors.white,
+                ),
+                LessonContent(
+                  tagalogWord: 'adobo',
+                  engWord: 'chicken',
+                  icon: FontAwesomeIcons.egg,
+                  color: Colors.white,
+                ),
+              ]),
+          Lessons(
+            name: 'Food three',
+            icon: FontAwesomeIcons.wineBottle,
+            color: Colors.purple,
+            backgroundColor: Colors.purpleAccent,
+          ),
+          Lessons(
+            name: 'Food four',
+            icon: FontAwesomeIcons.glassWhiskey,
+            color: Colors.purple,
+            backgroundColor: Colors.purpleAccent,
+          ),
+          Lessons(
+            name: 'Food five',
+            icon: FontAwesomeIcons.glassMartini,
+            color: Colors.purple,
+            backgroundColor: Colors.purpleAccent,
+          ),
+          Lessons(
+            name: 'Food six',
+            icon: FontAwesomeIcons.sink,
+            color: Colors.purple,
+            backgroundColor: Colors.purpleAccent,
+          ),
+        ],
+      ),
       Category(
-        name: 'Travel',
+        name: 'Alphabet',
+        icon: FontAwesomeIcons.wordpress,
+        color: Color.fromRGBO(124, 180, 2, 1.0),
+        backgroundColor: Color.fromRGBO(171, 236, 49, 1.0),
+        lessons: [
+          Lessons(
+            name: 'Lesson One',
+            icon: FontAwesomeIcons.pencilAlt,
+            color: Color.fromRGBO(124, 180, 2, 1.0),
+            backgroundColor: Color.fromRGBO(171, 236, 49, 1.0),
+          ),
+          Lessons(
+            name: 'Lesson Two',
+            icon: FontAwesomeIcons.bookOpen,
+            color: Color.fromRGBO(124, 180, 2, 1.0),
+            backgroundColor: Color.fromRGBO(171, 236, 49, 1.0),
+          ),
+          Lessons(
+            name: 'Lesson Three',
+            icon: FontAwesomeIcons.addressBook,
+            color: Color.fromRGBO(124, 180, 2, 1.0),
+            backgroundColor: Color.fromRGBO(171, 236, 49, 1.0),
+          ),
+          Lessons(
+            name: 'Lesson Four',
+            icon: FontAwesomeIcons.bookReader,
+            color: Color.fromRGBO(124, 180, 2, 1.0),
+            backgroundColor: Color.fromRGBO(171, 236, 49, 1.0),
+          ),
+          Lessons(
+            name: 'Lesson Five',
+            icon: FontAwesomeIcons.pen,
+            color: Color.fromRGBO(124, 180, 2, 1.0),
+            backgroundColor: Color.fromRGBO(171, 236, 49, 1.0),
+          ),
+          Lessons(
+            name: 'Lesson Six',
+            icon: FontAwesomeIcons.ruler,
+            color: Color.fromRGBO(124, 180, 2, 1.0),
+            backgroundColor: Color.fromRGBO(171, 236, 49, 1.0),
+          ),
+        ],
+      ),
+
+      Category(
+        name: 'Numbers',
+        icon: FontAwesomeIcons.personBooth,
+        color: Color.fromRGBO(0, 149, 85, 1.0),
+        backgroundColor: Colors.green[400],
+        lessons: [
+          Lessons(
+            name: 'Lesson One',
+            icon: FontAwesomeIcons.personBooth,
+            color: Color.fromRGBO(0, 149, 85, 1.0),
+            backgroundColor: Colors.green[400],
+          ),
+          Lessons(
+            name: 'Lesson Two',
+            icon: FontAwesomeIcons.tractor,
+            color: Color.fromRGBO(0, 149, 85, 1.0),
+            backgroundColor: Colors.green[400],
+          ),
+          Lessons(
+            name: 'Lesson Three',
+            icon: FontAwesomeIcons.spaceShuttle,
+            color: Color.fromRGBO(0, 149, 85, 1.0),
+            backgroundColor: Colors.green[400],
+          ),
+          Lessons(
+            name: 'Lesson Four',
+            icon: FontAwesomeIcons.plane,
+            color: Color.fromRGBO(0, 149, 85, 1.0),
+            backgroundColor: Colors.green[400],
+          ),
+          Lessons(
+            name: 'Lesson Five',
+            icon: FontAwesomeIcons.car,
+            color: Color.fromRGBO(0, 149, 85, 1.0),
+            backgroundColor: Colors.green[400],
+          ),
+          Lessons(
+            name: 'Lesson Six',
+            icon: FontAwesomeIcons.train,
+            color: Color.fromRGBO(0, 149, 85, 1.0),
+            backgroundColor: Colors.green[400],
+          ),
+        ],
+      ),
+      //TODO: Fill with lesson content
+      Category(
+        name: 'Days of the Week',
         icon: FontAwesomeIcons.plane,
         color: Colors.red[700],
         backgroundColor: Colors.redAccent,
         lessons: [
           Lessons(
-            name: 'Travel One',
+            name: 'Lesson One',
             icon: FontAwesomeIcons.bicycle,
             color: Colors.red[700],
             backgroundColor: Colors.redAccent,
           ),
           Lessons(
-            name: 'Travel Two',
+            name: 'Lesson Two',
             icon: FontAwesomeIcons.tractor,
             color: Colors.red[700],
             backgroundColor: Colors.redAccent,
           ),
           Lessons(
-            name: 'Travel Three',
+            name: 'Lesson Three',
             icon: FontAwesomeIcons.spaceShuttle,
             color: Colors.red[700],
             backgroundColor: Colors.redAccent,
           ),
           Lessons(
-            name: 'Travel Four',
+            name: 'Lesson Four',
             icon: FontAwesomeIcons.plane,
             color: Colors.red[700],
             backgroundColor: Colors.redAccent,
           ),
           Lessons(
-            name: 'Travel Five',
+            name: 'Lesson Five',
             icon: FontAwesomeIcons.car,
             color: Colors.red[700],
             backgroundColor: Colors.redAccent,
           ),
           Lessons(
-            name: 'Travel Six',
+            name: 'Lesson Six',
             icon: FontAwesomeIcons.train,
             color: Colors.red[700],
             backgroundColor: Colors.redAccent,
@@ -159,91 +250,75 @@ class Utils {
         ],
       ),
       Category(
-          name: 'People',
-          icon: FontAwesomeIcons.personBooth,
-          color: Color.fromRGBO(0, 149, 85, 1.0),
-          backgroundColor: Colors.green[400],
-          lessons: [
-            Lessons(
-              name: 'People One',
-              icon: FontAwesomeIcons.personBooth,
-              color: Color.fromRGBO(0, 149, 85, 1.0),
-              backgroundColor: Colors.green[400],
-            ),
-            Lessons(
-              name: 'People Two',
-              icon: FontAwesomeIcons.tractor,
-              color: Color.fromRGBO(0, 149, 85, 1.0),
-              backgroundColor: Colors.green[400],
-            ),
-            Lessons(
-              name: 'People Three',
-              icon: FontAwesomeIcons.spaceShuttle,
-              color: Color.fromRGBO(0, 149, 85, 1.0),
-              backgroundColor: Colors.green[400],
-            ),
-            Lessons(
-              name: 'People Four',
-              icon: FontAwesomeIcons.plane,
-              color: Color.fromRGBO(0, 149, 85, 1.0),
-              backgroundColor: Colors.green[400],
-            ),
-            Lessons(
-              name: 'People Five',
-              icon: FontAwesomeIcons.car,
-              color: Color.fromRGBO(0, 149, 85, 1.0),
-              backgroundColor: Colors.green[400],
-            ),
-            Lessons(
-              name: 'People Six',
-              icon: FontAwesomeIcons.train,
-              color: Color.fromRGBO(0, 149, 85, 1.0),
-              backgroundColor: Colors.green[400],
-            ),
-          ]),
+        name: 'Months',
+        icon: FontAwesomeIcons.paintBrush,
+        color: Colors.cyan,
+        backgroundColor: Colors.cyanAccent,
+        lessons: [
+          Lessons(
+            name: 'Months One',
+            icon: FontAwesomeIcons.calendar,
+            color: Colors.cyan,
+            backgroundColor: Colors.cyanAccent,
+          ),
+          Lessons(
+            name: 'Months Two',
+            icon: FontAwesomeIcons.calendarDay,
+            color: Colors.cyan,
+            backgroundColor: Colors.cyanAccent,
+          ),
+          Lessons(
+            name: 'Months Three',
+            icon: FontAwesomeIcons.calendarWeek,
+            color: Colors.cyan,
+            backgroundColor: Colors.cyanAccent,
+          ),
+        ],
+      ),
       Category(
-          name: 'Colours',
-          icon: FontAwesomeIcons.paintBrush,
-          color: Colors.deepOrange,
-          backgroundColor: Colors.orangeAccent,
-          lessons: [
-            Lessons(
-              name: 'Colours One',
-              icon: FontAwesomeIcons.bicycle,
-              color: Colors.deepOrange,
-              backgroundColor: Colors.orangeAccent,
-            ),
-            Lessons(
-              name: 'Colours Two',
-              icon: FontAwesomeIcons.tractor,
-              color: Colors.deepOrange,
-              backgroundColor: Colors.orangeAccent,
-            ),
-            Lessons(
-              name: 'Colours Three',
-              icon: FontAwesomeIcons.spaceShuttle,
-              color: Colors.deepOrange,
-              backgroundColor: Colors.orangeAccent,
-            ),
-            Lessons(
-              name: 'Colours Four',
-              icon: FontAwesomeIcons.plane,
-              color: Colors.deepOrange,
-              backgroundColor: Colors.orangeAccent,
-            ),
-            Lessons(
-              name: 'Colours Five',
-              icon: FontAwesomeIcons.car,
-              color: Colors.deepOrange,
-              backgroundColor: Colors.orangeAccent,
-            ),
-            Lessons(
-              name: 'Colours Six',
-              icon: FontAwesomeIcons.train,
-              color: Colors.deepOrange,
-              backgroundColor: Colors.orangeAccent,
-            ),
-          ]),
+        name: 'Colours',
+        icon: FontAwesomeIcons.paintBrush,
+        color: Colors.deepOrange,
+        backgroundColor: Colors.orangeAccent,
+        lessons: [
+          Lessons(
+            name: 'Colours One',
+            icon: FontAwesomeIcons.bicycle,
+            color: Colors.deepOrange,
+            backgroundColor: Colors.orangeAccent,
+          ),
+          Lessons(
+            name: 'Colours Two',
+            icon: FontAwesomeIcons.tractor,
+            color: Colors.deepOrange,
+            backgroundColor: Colors.orangeAccent,
+          ),
+          Lessons(
+            name: 'Colours Three',
+            icon: FontAwesomeIcons.spaceShuttle,
+            color: Colors.deepOrange,
+            backgroundColor: Colors.orangeAccent,
+          ),
+          Lessons(
+            name: 'Colours Four',
+            icon: FontAwesomeIcons.plane,
+            color: Colors.deepOrange,
+            backgroundColor: Colors.orangeAccent,
+          ),
+          Lessons(
+            name: 'Colours Five',
+            icon: FontAwesomeIcons.car,
+            color: Colors.deepOrange,
+            backgroundColor: Colors.orangeAccent,
+          ),
+          Lessons(
+            name: 'Colours Six',
+            icon: FontAwesomeIcons.train,
+            color: Colors.deepOrange,
+            backgroundColor: Colors.orangeAccent,
+          ),
+        ],
+      ),
     ];
   }
 }
