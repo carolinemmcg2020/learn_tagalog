d5fd48bf703748ca3c10741fc4760a6302ca1f00
caroline <caroline.mcgregor.2020@uni.strath.ac.uk>
2021-08-01 01:25:42 +0100

    Added Alert dialogue

--- lib/screens/topics.dart
+++ lib/screens/topics.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_alert/flutter_alert.dart';
 import 'package:learn_tagalog/helpers/utils.dart';
 import 'package:learn_tagalog/models/category.dart';
 import 'package:learn_tagalog/screens/lessondetail.dart';
@@ -16,7 +17,7 @@ class Topics extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ThemeColor(
-       /* decoration: BoxDecoration(
+        /* decoration: BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
@@ -56,7 +57,7 @@ class Topics extends StatelessWidget {
                                 width: 10,
                               ),
                               Padding(
-                                padding: const EdgeInsets.only(left:15.0),
+                                padding: const EdgeInsets.only(left: 15.0),
                                 child: Text(
                                   categories[index].name,
                                   style: TextStyle(
@@ -86,14 +87,47 @@ class Topics extends StatelessWidget {
                                 return LessonCard(
                                   lesson: categories[index].lessons[ind],
                                   onCardClick: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => LessonDetail(
-                                          lessons:
-                                              categories[index].lessons[ind],
-                                        ),
-                                      ),
+                                    showDialog(
+                                      context: context,
+                                      builder: (BuildContext alertContext) {
+                                        return AlertDialog(
+                                          title: Text('Are you sure?'),
+                                          content: Text(
+                                              'Are you ready to take this lesson?'),
+                                          actions: [
+                                            Row(
+                                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                              children: [
+                                                FlatButton(
+                                                  child: Text('Yes'),
+                                                  onPressed: () {
+                                                    //TODO: fill me in, send me to lesson page
+                                                    Navigator.pushReplacement(
+                                                      context,
+                                                      MaterialPageRoute(
+                                                        builder: (alertContext) =>
+                                                            LessonDetail(
+                                                              lessons: categories[index]
+                                                                  .lessons[ind],
+                                                            ),
+                                                      ),
+                                                    );
+                                                  },
+                                                ),
+                                                FlatButton(
+                                                  child: Text('No'),
+                                                  onPressed: () {
+                                                    //TODO: Close Alert
+                                                    Navigator.of(alertContext,
+                                                        rootNavigator: true)
+                                                        .pop();
+                                                  },
+                                                ),
+                                              ],
+                                            )
+                                          ],
+                                        );
+                                      },
                                     );
                                   },
                                 );
