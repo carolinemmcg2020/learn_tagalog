d42633a7e9e45c322d11e36a24997433a88a511d
Caroline <caroline.mcgregor.2020@uni.strath.ac.uk>
2021-08-04 20:58:40 +0100

    UI Updated, changed selected item colour

--- lib/bottom_nav_bar.dart
+++ lib/bottom_nav_bar.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:learn_tagalog/screens/collection.dart';
 import 'package:learn_tagalog/screens/profile.dart';
 import 'package:learn_tagalog/screens/topics_detail.dart';
 
-class BottomNavBar extends StatefulWidget {
+import 'helpers/utils.dart';
+import 'models/topic.dart';
 
+
+class BottomNavBar extends StatefulWidget {
   @override
   _BottomNavBarState createState() => _BottomNavBarState();
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
+  List<Topic> categories = Utils.getMockedCategories();
+
   int _selectedIndex = 0;
 
   final _pageOptions = [
@@ -18,25 +24,27 @@ class _BottomNavBarState extends State<BottomNavBar> {
     Profile(),
   ];
 
-  static const List<Widget> _pages = <Widget>[
+/*  static const List<Widget> _pages = <Widget>[
     Icon(
       Icons.home,
       size: 150,
     ),
     Icon(
-      Icons.pages,
+      FontAwesomeIcons.book,
       size: 150,
     ),
     Icon(
       Icons.person_rounded,
       size: 150,
     )
-  ];
+  ];*/
 
   void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+    setState(
+      () {
+        _selectedIndex = index;
+      },
+    );
   }
 
   @override
@@ -47,13 +55,14 @@ class _BottomNavBarState extends State<BottomNavBar> {
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Color(0xFFCE2029),
+        selectedItemColor: Color.fromRGBO(253, 202, 49, 1.0),
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Topics',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.pages),
+            icon: Icon(FontAwesomeIcons.book),
             label: 'Collection',
           ),
           BottomNavigationBarItem(
