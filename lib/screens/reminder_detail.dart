b98855d17579b19e158932fca5780e14ead2a96b
Caroline <caroline.mcgregor.2020@uni.strath.ac.uk>
2021-08-04 22:14:53 +0100

    Initial Commit.

---
+++ lib/screens/reminder_detail.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:learn_tagalog/widgets/theme_background_color.dart';
+
+class Reminder extends StatefulWidget {
+  @override
+  _ReminderState createState() => _ReminderState();
+}
+
+class _ReminderState extends State<Reminder> {
+  double _value = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: ThemeColor(
+        child: SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    width: 250.0,
+                    child: Center(
+                      child: Text(
+                        'Reminder',
+                        style: TextStyle(
+                          fontSize: 26.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(10.0, 0.0, 20.0, 10.0),
+                    child: Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Icon(
+                            FontAwesomeIcons.times,
+                            size: 40.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: Text(
+                  'How many lessons would you like to do in a week?',
+                  style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w300),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: 90.0,
+              ),
+              SliderTheme(
+                data: SliderTheme.of(context).copyWith(
+                  inactiveTrackColor: Colors.red,
+                ),
+                child: Slider(
+                  value: _value,
+                  min: 0,
+                  max: 20,
+                  divisions: 20,
+                  label: '$_value',
+                  onChanged: (value) {
+                    setState(() {
+                      _value = value;
+                    });
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(40.0),
+                child: Text(
+                  _value.toInt().toString() + ' days',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.amber,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 60.0,
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Set Reminder',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
