58bfcdec38ee06ff602d30d730a5ab5df8813eff
caroline <caroline.mcgregor.2020@uni.strath.ac.uk>
2021-08-16 17:51:37 +0100

    Initial Commit

---
+++ lib/widgets/custom_close_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomCloseButton extends StatelessWidget{
+  Function onTap;
+
+  CustomCloseButton({this.onTap});
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Row(
+      children: [
+        GestureDetector(
+          onTap: onTap,
+          child: Icon(
+            FontAwesomeIcons.times,
+            size: 40.0,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    );
+  }
+
+}
