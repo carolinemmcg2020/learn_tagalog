1f50f8ba6e564e2eed3aff56b9012257a5abe82e
caroline <caroline.mcgregor.2020@uni.strath.ac.uk>
2021-08-16 16:35:14 +0100

    Refactoring

--- lib/screens/header_page.dart
+++ lib/screens/header_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -6,12 +8,24 @@ import 'package:learn_tagalog/models/loginusermodel.dart';
 import 'package:learn_tagalog/services/google_login_service.dart';
 import 'package:provider/provider.dart';
 
-class HeaderPage extends StatelessWidget {
+class HeaderPage extends StatefulWidget {
   static const keyDarkMode = 'key-dark-mode';
-
   bool showProfilePic;
 
   HeaderPage({this.showProfilePic});
+
+  @override
+  _HeaderPageState createState() => _HeaderPageState();
+}
+
+class _HeaderPageState extends State<HeaderPage> {
+  FirebaseAuth auth = FirebaseAuth.instance;
+  final userRef = FirebaseFirestore.instance.collection("users");
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) => Column(
@@ -26,7 +40,7 @@ class HeaderPage extends StatelessWidget {
       );
 
   Widget buildDarkMode() => SwitchSettingsTile(
-        settingKey: keyDarkMode,
+        settingKey: HeaderPage.keyDarkMode,
         leading: IconWidget(
           icon: Icons.data_usage_rounded,
           color: Color(0xFF642ef3),
@@ -39,50 +53,56 @@ class HeaderPage extends StatelessWidget {
     final GoogleLoginService loginService =
         Provider.of<GoogleLoginService>(context, listen: false);
 
-
     LoginUserModel googleUserModel = loginService.loggedInUserModel;
 
     String imgPath = googleUserModel != null ? googleUserModel.photoUrl : '';
-    String userName = googleUserModel != null ? googleUserModel.displayName : '';
+    String userName =
+        googleUserModel != null ? googleUserModel.displayName : '';
     String userEmail = googleUserModel != null ? googleUserModel.email : '';
 
     bool showUserMapBadge = userName.isNotEmpty && userEmail.isNotEmpty;
 
-    return this.showProfilePic && imgPath.isNotEmpty && showUserMapBadge
-        ? SimpleSettingsTile(
-            title: userName,
-            subtitle: userEmail,
-            leading: ClipOval(
-              child: Image.network(imgPath),
-            ),
-            onTap: () => ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('Clicked User Profile'),
-              ),
-            ),
-            child: SettingsScreen(
-              children: [buildUserDetails(context)],
-            ),
-          )
-        : SimpleSettingsTile(
-            title: 'User Name',
-            subtitle: 'User Email',
-            leading: ClipOval(
-              child: IconWidget(
-                icon: FontAwesomeIcons.user,
-                color: Colors.purpleAccent,
-              ),
-            ),
-            onTap: () => ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('Clicked User Profile'),
-              ),
-              ),
-            );
-            // child: SettingsScreen(
-            //   children: [buildUserDetails(context)],
-            // ),
-
+    if (showUserMapBadge) {
+      return SimpleSettingsTile(
+        title: userEmail,
+        subtitle: 'Signed using Google',
+        leading: ClipOval(
+          child: Image.network(imgPath),
+        ),
+        onTap: () => ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Clicked User Profile'),
+          ),
+        ),
+      );
+    } else if (auth.currentUser != null) {
+      return SimpleSettingsTile(
+        title: auth.currentUser.email,
+        subtitle: "Signed in using E-mail",
+        leading: ClipOval(
+          child: IconWidget(
+            icon: FontAwesomeIcons.user,
+            color: Colors.purple,
+          ),
+        ),
+      );
+    } else {
+      return SimpleSettingsTile(
+        title: 'User Email',
+        subtitle: 'Sign-in method',
+        leading: ClipOval(
+          child: IconWidget(
+            icon: FontAwesomeIcons.user,
+            color: Colors.purpleAccent,
+          ),
+        ),
+        onTap: () => ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Clicked'),
+          ),
+        ),
+      );
+    }
   }
 
   Widget buildHeader() => Center(
