f13c60bea9425bcc39df8ca308ab382899209918
caroline <caroline.mcgregor.2020@uni.strath.ac.uk>
2021-08-18 00:18:46 +0100

    Refactoring

--- lib/screens/end_of_lesson_quiz_detail.dart
+++ lib/screens/end_of_lesson_quiz_detail.dart
@@ -1,8 +1,9 @@
-import 'dart:math';
+import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:learn_tagalog/models/lesson.dart';
+import 'package:learn_tagalog/models/quiz_brain.dart';
 import 'package:learn_tagalog/screens/results_detail.dart';
 import 'package:learn_tagalog/widgets/quiz_button.dart';
 import 'package:learn_tagalog/widgets/theme_background_color.dart';
@@ -21,27 +22,18 @@ class EndOfLessonQuizDetail extends StatefulWidget {
 class _EndOfLessonQuizDetailState extends State<EndOfLessonQuizDetail> {
   double _progress = 0.0;
   int index = 0;
-  bool checked;
   int correctAnswerCount = 0;
   AudioPlayer player;
 
-  //TODO Fix Colour Button Check
-  Color buttonColorWrong = Color.fromRGBO(206, 3, 3, 1.0);
-  Color buttonColorRight = Color.fromRGBO(64, 158, 0, 1.0);
-
-  Color buttonColor = Color.fromRGBO(253, 202, 49, 1.0);
-  Color buttonColor1 = Color.fromRGBO(253, 202, 49, 1.0);
-  Color buttonColor2 = Color.fromRGBO(253, 202, 49, 1.0);
-  Color buttonColor3 = Color.fromRGBO(253, 202, 49, 1.0);
+  QuizBrain quizBrain;
 
   @override
   void initState() {
-    var option1 = getRandomElement(widget.questionContent);
-    var option2 = getRandomElement(widget.questionContent);
-    var option3 = getRandomElement(widget.questionContent);
-    checkOptions(option1, option2, option3);
     super.initState();
     player = AudioPlayer();
+    quizBrain = QuizBrain();
+    quizBrain.lessonBank.addAll(widget.questionContent);
+    quizBrain.checkOptions();
   }
 
   @override
@@ -50,112 +42,64 @@ class _EndOfLessonQuizDetailState extends State<EndOfLessonQuizDetail> {
     super.dispose();
   }
 
-  T getRandomElement<T>(List<T> list) {
-    final random = new Random();
-    var i = random.nextInt(list.length);
-    if (index == i) {
-      i++;
-    }
-    print(i);
-    return list[i];
-  }
-
   Future playOption(String optionAudio) async {
     await player.setAsset(optionAudio);
     player.play();
   }
 
-  void nextQuestion() {
-    setState(
-      () {
-        if (index >= widget.questionContent.length - 1) {
-          print('end of list');
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (context) => ResultsDetail(
-                userResults: correctAnswerCount,
-                numOfQs: widget.questionContent,
-              ),
+  showResults() {
+    if (quizBrain.isFinished() == true) {
+      Future.delayed(Duration(seconds: 1), () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ResultsDetail(
+              userResults: correctAnswerCount,
+              numOfQs: widget.questionContent,
             ),
-          );
-        } else {
-          if (index < widget.questionContent.length - 1) {
-            index++;
-            _progress++;
-          }
-        }
-      },
-    );
-  }
-
-  void resetQuiz() {
-    setState(() {
-      index = 0;
-      _progress = 0;
-    });
+          ),
+        );
+      });
+    }
   }
 
   void checkAnswer(String userAnswer) {
     setState(
       () {
-        if (userAnswer.contains(widget.questionContent[index].engWord) ==
-            true) {
-          print('Correct answer');
-          checked = true;
+        if (userAnswer.contains(quizBrain.getEnglishText()) == true) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                'Correct answer',
+                style: TextStyle(
+                  color: Colors.green,
+                ),
+              ),
+              duration: Duration(seconds: 1),
+            ),
+          );
           correctAnswerCount++;
+          _progress++;
         } else {
-          print('Incorrect answer!');
-          checked = false;
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                'Wrong answer',
+                style: TextStyle(color: Colors.red),
+              ),
+              duration: Duration(milliseconds: 500),
+            ),
+          );
         }
-        buttonReset();
-        nextQuestion();
+        quizBrain.nextQuestion();
+        quizBrain.checkOptions();
       },
     );
-  }
-
-  //TODO: check this
-  void checkOptions(Lesson option1, Lesson option2, Lesson option3) {
-    if (option1 == option2) {
-      print("op1 and op2  are the same");
-    }
-    if (option2 == option3) {
-      print("option2 and opt 3  are the same");
-      option2;
-    }
-    if (option3 == option1) {
-      print("op3 and op1 are the same");
-      option3;
-    }
-  }
-
-  //TODO Fix Colour Button Check
-  void buttonColourCheck(Color colourChange) {
-    setState(
-      () {
-        if (checked == true) {
-          return colourChange = buttonColorRight;
-        } else {
-          return colourChange = Color.fromRGBO(206, 3, 3, 1.0);
-        }
-      },
-    );
-  }
-
-  void buttonReset() {
-    buttonColor = Color.fromRGBO(253, 202, 49, 1.0);
-    buttonColor1 = Color.fromRGBO(253, 202, 49, 1.0);
-    buttonColor2 = Color.fromRGBO(253, 202, 49, 1.0);
-    buttonColor3 = Color.fromRGBO(253, 202, 49, 1.0);
+    showResults();
   }
 
   @override
   Widget build(BuildContext context) {
-    // var option = getRandomElement(widget.questionContent);
-    var option1 = getRandomElement(widget.questionContent);
-    var option2 = getRandomElement(widget.questionContent);
-    var option3 = getRandomElement(widget.questionContent);
-
     return Scaffold(
       body: ThemeColor(
         child: SafeArea(
@@ -192,12 +136,11 @@ class _EndOfLessonQuizDetailState extends State<EndOfLessonQuizDetail> {
                 child: Padding(
                   padding: const EdgeInsets.all(15.0),
                   child: Center(
-                    //TODO: put this in a if statement based on variable type
                     child: Text(
                       'What is the ' +
-                          widget.questionContent[index].type +
+                          quizBrain.getType() +
                           ' for ' +
-                          widget.questionContent[index].name +
+                          quizBrain.getTagalogText() +
                           '?',
                       style: TextStyle(fontSize: 23.0),
                       textAlign: TextAlign.center,
@@ -207,17 +150,14 @@ class _EndOfLessonQuizDetailState extends State<EndOfLessonQuizDetail> {
               ),
               ListView(
                 shrinkWrap: true,
-                reverse: true,
+                reverse: false,
                 children: [
                   QuizButton(
-                    buttonTxt: widget.questionContent[index].engWord,
-                    txtColor: buttonColor,
+                    buttonTxt: quizBrain.getEnglishText(),
                     onTap: () {
                       setState(() {
-                        playOption(widget.questionContent[index].audio);
-                        checkAnswer(widget.questionContent[index].engWord);
-                        getRandomElement(widget.questionContent);
-                        buttonColourCheck(buttonColor);
+                        playOption(quizBrain.getAudio());
+                        checkAnswer(quizBrain.getEnglishText());
                       });
                     },
                   ),
@@ -225,14 +165,15 @@ class _EndOfLessonQuizDetailState extends State<EndOfLessonQuizDetail> {
                     height: 30.0,
                   ),
                   QuizButton(
-                    buttonTxt: option1.engWord,
-                    txtColor: buttonColor1,
+                    buttonTxt:
+                        quizBrain.lessonBank[quizBrain.getOpt1Index()].engWord,
                     onTap: () {
                       setState(
                         () {
-                          playOption(option1.audio);
-                          checkAnswer(option1.engWord);
-                          buttonColourCheck(buttonColor1);
+                          checkAnswer(quizBrain
+                              .lessonBank[quizBrain.getOpt1Index()].engWord);
+                          playOption(quizBrain
+                              .lessonBank[quizBrain.getOpt1Index()].audio);
                         },
                       );
                     },
@@ -241,19 +182,15 @@ class _EndOfLessonQuizDetailState extends State<EndOfLessonQuizDetail> {
                     height: 30.0,
                   ),
                   QuizButton(
-                    buttonTxt: option2.engWord,
-                    txtColor: buttonColor2,
+                    buttonTxt:
+                        quizBrain.lessonBank[quizBrain.getOpt2Index()].engWord,
                     onTap: () {
                       setState(
                         () {
-                          playOption(option2.audio);
-                          checkAnswer(option2.engWord);
-                          // buttonColourCheck(buttonColor2);
-                          if (checked == true) {
-                            buttonColor2 = buttonColorRight;
-                          } else {
-                            buttonColor2 = buttonColorWrong;
-                          }
+                          checkAnswer(quizBrain
+                              .lessonBank[quizBrain.getOpt2Index()].engWord);
+                          playOption(quizBrain
+                              .lessonBank[quizBrain.getOpt2Index()].audio);
                         },
                       );
                     },
@@ -262,100 +199,24 @@ class _EndOfLessonQuizDetailState extends State<EndOfLessonQuizDetail> {
                     height: 30.0,
                   ),
                   QuizButton(
-                    buttonTxt: option3.engWord,
-                    txtColor: buttonColor3,
+                    buttonTxt:
+                        quizBrain.lessonBank[quizBrain.getOpt3Index()].engWord,
                     onTap: () {
                       setState(
                         () {
-                          playOption(option3.audio);
-                          checkAnswer(option3.engWord);
-                          buttonColourCheck(buttonColor3);
-                          if (checked == true) {
-                            buttonColor3 = buttonColorRight;
-                          } else {
-                            buttonColor3 = buttonColorWrong;
-                          }
+                          checkAnswer(quizBrain
+                              .lessonBank[quizBrain.getOpt3Index()].engWord);
+                          playOption(quizBrain
+                              .lessonBank[quizBrain.getOpt3Index()].audio);
                         },
                       );
                     },
                   ),
+                  SizedBox(
+                    height: 40,
+                  )
                 ],
               ),
-              //TODO: remove this dead code
-              /* QuizButton(
-                buttonTxt: widget.questionContent[index].engWord,
-                txtColor: buttonColor,
-                onTap: () {
-                  setState(() {
-                    playOption(widget.questionContent[index].audio);
-                    checkAnswer(widget.questionContent[index].engWord);
-                    getRandomElement(widget.questionContent);
-                    buttonColourCheck(buttonColor);
-
-                  });
-                },
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              QuizButton(
-                buttonTxt: option1.engWord,
-                txtColor: buttonColor1,
-                onTap: () {
-                  setState(
-                    () {
-                      playOption(option1.audio);
-                      checkAnswer(option1.engWord);
-                      buttonColourCheck(buttonColor1);
-                    },
-                  );
-                },
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              QuizButton(
-                buttonTxt: option2.engWord,
-                txtColor: buttonColor2,
-                onTap: () {
-                  setState(
-                    () {
-                      playOption(option2.audio);
-                      checkAnswer(option2.engWord);
-                      // buttonColourCheck(buttonColor2);
-                      if (checked == true) {
-                        buttonColor2 = buttonColorRight;
-                      } else {
-                        buttonColor2 = buttonColorWrong;
-                      }
-                    },
-                  );
-                },
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              QuizButton(
-                buttonTxt: option3.engWord,
-                txtColor: buttonColor3,
-                onTap: () {
-                  setState(
-                    () {
-                      playOption(option3.audio);
-                      checkAnswer(option3.engWord);
-                       buttonColourCheck(buttonColor3);
-                      */ /*if (checked == true) {
-                        buttonColor3 = buttonColorRight;
-                      } else {
-                        buttonColor3 = buttonColorWrong;
-                      }*/ /*
-                    },
-                  );
-                },
-              ),
-              SizedBox(
-                height: 50.0,
-              )*/
             ],
           ),
         ),
