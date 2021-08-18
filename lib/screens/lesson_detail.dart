3dfe892b48d307df95419caa69c580b16f3275e2
caroline <caroline.mcgregor.2020@uni.strath.ac.uk>
2021-08-18 12:56:26 +0100

    Refactoring

--- lib/screens/lesson_detail.dart
+++ lib/screens/lesson_detail.dart
@@ -7,6 +7,7 @@ import 'package:learn_tagalog/bottom_nav_bar.dart';
 import 'package:learn_tagalog/models/lesson.dart';
 import 'package:learn_tagalog/models/topic.dart';
 import 'package:learn_tagalog/screens/end_of_lesson_quiz_detail.dart';
+import 'package:learn_tagalog/utilities/lesson_logic.dart';
 import 'package:learn_tagalog/widgets/custom_alert_dialog.dart';
 import 'package:learn_tagalog/widgets/custom_close_button.dart';
 import 'package:learn_tagalog/widgets/custom_lesson_icon.dart';
@@ -25,13 +26,15 @@ class _LessonDetailState extends State<LessonDetail> {
   AudioPlayer player;
   Topic category;
 
-  int index = 0;
-  double percent = 0.0;
+
+  LessonLogic lessonLogic;
 
   @override
   void initState() {
     super.initState();
     player = AudioPlayer();
+    lessonLogic = LessonLogic();
+    lessonLogic.lessonContent.addAll(widget.lessons.lessonContent);
   }
 
   @override
@@ -40,86 +43,20 @@ class _LessonDetailState extends State<LessonDetail> {
     super.dispose();
   }
 
-  void reset() {
-    index = 0;
-    percent = 0.0;
-  }
-
-  void nextWord() {
-    setState(
-      () {
-        if (index >= widget.lessons.lessonContent.length - 1) {
-          print('button pressed');
-          finishedAlertDialog();
-        } else {
-          if (index < widget.lessons.lessonContent.length - 1) {
-            index++;
-          }
-        }
-      },
-    );
-  }
-
-  double phraseFontSize(){
-    if(widget.lessons.lessonContent[index].type == "phrase"){
-      return 28.0;
-    } else {
-      return 40.0;
-    }
-  }
-
-  void finishedAlertDialog() {
-    showAlert(
-      context: context,
-      title: 'Review lesson?',
-      body: 'Test your knowledge by answering questions about this lesson.',
-      actions: [
-        AlertAction(
-          text: 'Yes',
-          onPressed: () {
-            setState(() {
-              index = 0;
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => EndOfLessonQuizDetail(
-                    questionContent: widget.lessons.lessonContent,
-                    quizTitle: widget.lessons.name,
-                    //index: index,
-                  ),
-                ),
-              );
-            });
-          },
-        ),
-        AlertAction(
-          text: 'Redo',
-          onPressed: () {
-            setState(
-                  () {
-                reset();
-              },
-            );
-            print('pressed redo' + '' + index.toString());
-          },
-        )
-      ],
-    );
-  }
-
-  alertDialog(BuildContext context) {
+  exitDialog(BuildContext context) {
     showDialog(
         context: context,
         builder: (BuildContext alertContext) {
           return CustomAlertDialog(
             title: "Exit this lesson",
-            content: "Are you sure want to exit this lesson? You will loose progress, if you confirm",
+            content:
+                "Are you sure want to exit this lesson? You will loose progress, if you confirm",
             button1Text: 'Yes',
             btn1Func: () {
               Navigator.pushAndRemoveUntil(
                   context,
                   MaterialPageRoute(builder: (context) => BottomNavBar()),
-                      (route) => false);
+                  (route) => false);
             },
             button2Text: 'No',
             btn2Func: () {
@@ -163,21 +100,19 @@ class _LessonDetailState extends State<LessonDetail> {
                       lineHeight: 10.0,
                       backgroundColor: Colors.white,
                       progressColor: Colors.amber,
-                      percent: percent /
-                          widget.lessons.lessonContent.length.toDouble(),
+                      percent: lessonLogic.getProgress(),
                     ),
                   ),
                   Container(
-                    padding: EdgeInsets.fromLTRB(10.0, 0.0, 20.0, 10.0),
-                    // margin: EdgeInsets.symmetric(vertical: 10.0),
-                    child:CustomCloseButton(
-                      onTap: (){
-                        setState(() {
-                          alertDialog(context);
-                        });
-                      },
-                    )
-                  ),
+                      padding: EdgeInsets.fromLTRB(10.0, 0.0, 20.0, 10.0),
+                      // margin: EdgeInsets.symmetric(vertical: 10.0),
+                      child: CustomCloseButton(
+                        onTap: () {
+                          setState(() {
+                            exitDialog(context);
+                          });
+                        },
+                      )),
                 ],
               ),
               Container(
@@ -186,30 +121,25 @@ class _LessonDetailState extends State<LessonDetail> {
                 child: Column(
                   children: [
                     CustomLessonIcon(
-                      onTap: () async {
-                        await player.setAsset(
-                            widget.lessons.lessonContent[index].audio);
-                        await player.setSpeed(1);
-                        player.play();
+                      onTap: () {
+                        lessonLogic.playAudio();
                       },
-                      onDoubleTap: () async{
-                        await player.setAsset(
-                            widget.lessons.lessonContent[index].audio);
-                        await player.setSpeed(0.5);
-                        player.play();
+                      onDoubleTap: () {
+                        print("double tapped");
+                        lessonLogic.playHalfSpeed();
                       },
-                      icon: widget.lessons.lessonContent[index].icon,
-                      iconColor: widget.lessons.lessonContent[index].color,
+                      icon: lessonLogic.getIcon(),
+                      iconColor: lessonLogic.getIconColor(),
                       iconSize: 140.0,
                       glowRadius: 120.0,
                     ),
                     Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: Text(
-                        widget.lessons.lessonContent[index].name,
+                        lessonLogic.getTagalogWord(),
                         textAlign: TextAlign.center,
                         style: TextStyle(
-                          fontSize: phraseFontSize(),
+                          fontSize: lessonLogic.phraseFontSize(),
                           fontWeight: FontWeight.bold,
                           color: Colors.white,
                         ),
@@ -218,7 +148,7 @@ class _LessonDetailState extends State<LessonDetail> {
                     Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: Text(
-                        widget.lessons.lessonContent[index].engWord,
+                        lessonLogic.getEngWord(),
                         textAlign: TextAlign.center,
                         style: TextStyle(
                           fontWeight: FontWeight.w500,
@@ -231,11 +161,21 @@ class _LessonDetailState extends State<LessonDetail> {
                       padding: EdgeInsets.all(10.0),
                       child: GestureDetector(
                         onTap: () {
-                          nextWord();
                           player.stop();
+
                           setState(
                             () {
-                              percent++;
+                              lessonLogic.nextWord();
+
+                              lessonLogic.finishedAlertDialog(
+                                  context,
+                                  EndOfLessonQuizDetail(
+                                    questionContent:
+                                    widget.lessons.lessonContent,
+                                    quizTitle: widget.lessons.name,
+                                  )
+                                 );
+                             // percent++;
                             },
                           );
                         },
